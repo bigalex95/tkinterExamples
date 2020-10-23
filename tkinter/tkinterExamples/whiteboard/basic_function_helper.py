fde3be8b20f1fcc74034157a869daae2d419d14a
bigalex95 <alibek060395@gmail.com>
2020-10-23 16:52:14 +0300

    C/C++ window min/max

---
+++ tkinter/tkinterExamples/whiteboard/basic_function_helper.py
@@ -0,0 +1,17 @@
+"""
+Define the C-variables and functions from the C-files that are needed in Python
+"""
+from ctypes import *
+import sys
+print(sys.platform)
+lib_path = './basic_function_%s.so' % (sys.platform)
+print(lib_path)
+basic_function_lib = CDLL(lib_path)
+print(basic_function_lib)
+# try:
+#     basic_function_lib = CDLL(lib_path)
+# except:
+#     print('OS %s not recognized' % (sys.platform))
+
+# python_c_square = basic_function_lib.c_square
+# python_c_square.restype = None
--- tkinter/tkinterExamples/whiteboard/winMinMax.c
+++ tkinter/tkinterExamples/whiteboard/winMinMax.c
@@ -1,5 +1,10 @@
 #include <windows.h>
+#include <unistd.h>
+int main(){
+    HWND canvasWindow = FindWindowA(0, "OpenCV/Numpy normal");
+    sleep(15);
+    ShowWindow(canvasWindow, SW_MINIMIZE);
+    sleep(5);
+    ShowWindow(canvasWindow, SW_SHOWMAXIMIZED);
+}
 
-HWND canvasWindow = FindWindowEx(0, 0, "tk", 0);
-
-ShowWindow(canvasWindow, SW_MINIMIZE)
